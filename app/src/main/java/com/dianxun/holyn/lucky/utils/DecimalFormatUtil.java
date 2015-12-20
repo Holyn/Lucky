3aa5195bf42f9caa7cfaf8d82fa4d8364343ad70
Holyn <328550680@qq.com>
2015-12-20 16:47:37 +0800

    操蛋的重命名包名，要先删除后添加

---
+++ app/src/main/java/com/dianxun/holyn/lucky/utils/DecimalFormatUtil.java
@@ -0,0 +1,25 @@
+package com.dianxun.holyn.lucky.utils;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+/**
+ * Created by holyn on 2015/12/18.
+ */
+public class DecimalFormatUtil {
+    /** 求小数点后两位 要四舍五入 */
+    public static String getAfterTwo(double num){
+        DecimalFormat df   = new DecimalFormat();
+        df.setMaximumFractionDigits(2);//多少位
+//        df.setGroupingSize(0);
+        return  df.format(num);
+    }
+    /** 求小数点后两位 不四舍五入 */
+    public static String getAfterTwoFloor(double num){
+        DecimalFormat df   = new DecimalFormat();
+        df.setMaximumFractionDigits(2);//多少位
+        df.setGroupingSize(0);
+        df.setRoundingMode(RoundingMode.FLOOR);//不要四舍五入
+        return  df.format(num);
+    }
+}
